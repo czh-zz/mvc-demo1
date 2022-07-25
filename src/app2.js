cdddc951545a012049c72555d05a513fcdce1308
chenzh <515740090@qq.com>
2022-07-25 22:48:19 +0800

    实现mvc-demo1前2个模块

---
+++ src/app2.js
@@ -0,0 +1,24 @@
+import $ from 'jquery'
+
+const $tabBar = $('#app2 .tab-bar')
+const $tabContent = $('#app2 .tab-content')
+//监听li元素事件，并获取第几个li
+$tabBar.on('click', 'li', (e) => {
+    const $li = $(e.currentTarget);
+    $li.addClass("selected")
+        .siblings().removeClass("selected")
+    const index = $li.index()
+    $tabContent.children()
+        //第index个显示
+        .eq(index).addClass("active")
+        //其他不显示
+        .siblings().removeClass("active")
+
+    /*不建议用.eq(index).show()
+            .siblings().hide()
+            和
+    .eq(index).css({ diaplay: 'block' })
+    .siblings().css({ display: 'none' })*/
+});
+//默认显示，触发事件（也可在index页面的li分别加elected，active）
+$tabBar.children().eq(0).trigger('click')
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './reset.css'
+import './app1.css'
+import './app2.css'
+import './global.css'
+import './app1.js'
+import './app2.js'
